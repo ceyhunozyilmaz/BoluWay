aa3d14ec39ac539a62013771e4c77dd01ed6548d
ceyhunozyilmaz <sanatsalsaat18@gmail.com>
2024-05-01 13:06:16 +0300

    login verification with google enabled

---
+++ src/app/api/products/route.ts
@@ -0,0 +1,27 @@
+import { prisma } from "@/utils/connect";
+import { NextRequest, NextResponse } from "next/server";
+
+// FETCH ALL PRODUCTS
+export const GET = async (req: NextRequest) => {
+  const { searchParams } = new URL(req.url);
+  const cat = searchParams.get("cat");
+
+  try {
+    const product = await prisma.product.findMany({
+      where: {
+        ...(cat ? { catSlug: cat } : { isFeatured: true }),
+      },
+    });
+    return new NextResponse(JSON.stringify(product), { status: 200 });
+  } catch (err) {
+    console.log(err);
+    return new NextResponse(
+      JSON.stringify({ message: "Something went wrong!" }),
+      { status: 500 }
+    );
+  }
+};
+
+export const POST = () => {
+  return new NextResponse("Hello", { status: 200 });
+};
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,11 +3,12 @@ import React from "react";
 import Menu from "./Menu";
 import CartIcon from "./CartIcon";
 import Image from "next/image";
+import UserLinks from "./UserLinks";
 
 const Navbar = () => {
   const user = false;
   return (
-    <div className="h-12 text-red-500 p-4 flex items-center justify-between border-b-2 border-b-red-500 uppercase md:h-24 lg:px-20 xl:px-40">
+    <div className="h-12 text-red-500 p-4 flex items-center justify-between border-b-2 border-b-red-500 uppercase md:h-24  lg:px-20 xl:px-40">
       {/* LEFT LINKS */}
       <div className="hidden md:flex gap-4 flex-1">
         <Link href="/">Homepage</Link>
@@ -23,16 +24,12 @@ const Navbar = () => {
         <Menu />
       </div>
       {/* RIGHT LINKS */}
-      <div className="hidden md:flex gap-4 items-cente justify-end flex-1">
+      <div className="hidden md:flex gap-4 items-cente justify-end flex-2">
         <div className="md:absolute top-3 right-2 lg:static flex item-center gap-2 cursor-pointer bg-lime-400 px-1 rounded-md">
           <Image src="/phone.png" alt="" width={20} height={20} />
           <span className="text-black">0374 123 14 14</span>
         </div>
-        {!user ? (
-          <Link href="/login">Login</Link>
-        ) : (
-          <Link href="/orders">Orders</Link>
-        )}
+        <UserLinks />
         <CartIcon />
       </div>
     </div>
