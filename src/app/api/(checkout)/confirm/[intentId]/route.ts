aee9ff9b5d60598f1e0baaa4e90088b267a62cbc
ceyhunozyilmaz <sanatsalsaat18@gmail.com>
2024-05-12 16:25:33 +0300

    routets update

--- src/app/api/(checkout)/confirm/[intentId]/route.ts
+++ src/app/api/(checkout)/confirm/[intentId]/route.ts
@@ -1,23 +1,27 @@
 import { prisma } from "@/utils/connect";
 import { NextResponse } from "next/server";
 
-export const PUT = async ({ params }: { params: { intentId: string } }) => {
+export const PUT = async (
+  request: Request,
+  { params }: { params: { intentId: string } }
+) => {
   const { intentId } = params;
+
   try {
     await prisma.order.update({
       where: {
         intent_id: intentId,
       },
-      data: { status: "Hazırlanıyor!" },
+      data: { status: "Sipariş Hazırlanıyor!" },
     });
     return new NextResponse(
-      JSON.stringify({ message: "Sipariş Güncellendi " }),
+      JSON.stringify({ message: "Sipariş Güncellendi" }),
       { status: 200 }
     );
   } catch (err) {
     console.log(err);
     return new NextResponse(
-      JSON.stringify({ message: "Bir şeyler yanlış gitti!" }),
+      JSON.stringify({ message: "Something went wrong!" }),
       { status: 500 }
     );
   }
