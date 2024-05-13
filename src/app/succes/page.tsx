3d36d62fc619c4649d274b0e799c5605564b2f74
ceyhunozyilmaz <sanatsalsaat18@gmail.com>
2024-05-13 14:13:06 +0300

    succes hidden test

--- src/app/succes/page.tsx
+++ src/app/succes/page.tsx
@@ -1,41 +1,41 @@
-import { useRouter } from "next/router";
-import React, { useEffect } from "react";
-import ConfettiExplosion from "react-confetti-explosion";
+// import { useRouter } from "next/navigation";
+// import React, { useEffect } from "react";
+// import ConfettiExplosion from "react-confetti-explosion";
 
-const SuccessPage = () => {
-  const router = useRouter();
-  const payment_intent = router.query.payment_intent;
+// const SuccessPage = () => {
+//   const router = useRouter();
+// const payment_intent = router.query.payment_intent as string;
 
-  useEffect(() => {
-    const makeRequest = async () => {
-      try {
-        if (payment_intent) {
-          await fetch(`http://localhost:3000/api/confirm/${payment_intent}`, {
-            method: "PUT",
-          });
-          setTimeout(() => {
-            router.push("/orders");
-          }, 5000);
-        }
-      } catch (err) {
-        console.log(err);
-      }
-    };
+//   useEffect(() => {
+//     const makeRequest = async () => {
+//       try {
+//         if (payment_intent) {
+//           await fetch(`http://localhost:3000/api/confirm/${payment_intent}`, {
+//             method: "PUT",
+//           });
+//           setTimeout(() => {
+//             router.push("/orders");
+//           }, 5000);
+//         }
+//       } catch (err) {
+//         console.log(err);
+//       }
+//     };
 
-    makeRequest();
-  }, [payment_intent, router]);
+//     makeRequest();
+//   }, [payment_intent, router]);
 
-  return (
-    <>
-      <div className="min-h-[calc(100vh-6rem)] md:min-h-[calc(100vh-15rem)] flex items-center justify-center text-center text-2xl text-green-700">
-        <p className="max-w-[600px]">
-          Ödeme başarılı. Siparişler sayfasına yönlendiriliyorsunuz. Lütfen
-          sayfayı kapatmayın.
-        </p>
-        <ConfettiExplosion className="absolute m-auto" />
-      </div>
-    </>
-  );
-};
+//   return (
+//     <>
+//       <div className="min-h-[calc(100vh-6rem)] md:min-h-[calc(100vh-15rem)] flex items-center justify-center text-center text-2xl text-green-700">
+//         <p className="max-w-[600px]">
+//           Ödeme başarılı. Siparişler sayfasına yönlendiriliyorsunuz. Lütfen
+//           sayfayı kapatmayın.
+//         </p>
+//         <ConfettiExplosion className="absolute m-auto" />
+//       </div>
+//     </>
+//   );
+// };
 
-export default SuccessPage;
+// export default SuccessPage;
